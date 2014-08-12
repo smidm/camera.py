e101d57f4db4c7dcd7af0d9662c502bbdbacdb2a
Matěj Šmíd <smidm@cmp.felk.cvut.cz>
2014-08-12 17:14:01 +0200

    now installable as a python package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Camera',
+    version='0.1',
+    py_modules=['camera'],
+    license='The MIT License',
+    long_description=open('README.txt').read(),
+)
